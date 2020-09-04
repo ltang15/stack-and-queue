9c2ea85cd4c315f8031b0ade94ed899786e6be3e
ltang15 <62497601+ltang15@users.noreply.github.com>
2020-09-04 01:24:58 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "stack.h"
+#include "sq_test.h"
+#include "test_list.h"
+using namespace std;
+
+int main() {
+    queue_test();
+    //stack_test();
+    // test_list ();
+
+    return 0;
+}
